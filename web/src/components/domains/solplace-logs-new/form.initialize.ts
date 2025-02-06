bbcd1eaf359aeb3df157b681d35f3544ddf638a1
MingseoYa <alstj3758@naver.com>
2025-02-06 23:23:09 +0900

    Feat: 솔플레이스 등록 페이지 컴포넌트 구조 설정 및 퍼블리싱

---
+++ web/src/components/domains/solplace-logs-new/form.initialize.ts
@@ -0,0 +1,74 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useDeviceSetting } from "@/commons/settings/device-setting/hook";
+import { UseFormReturn } from "react-hook-form";
+import { ISchema } from "./form.schema";
+import { useImageStore } from "@/commons/stores/image-store";
+import { gql, useMutation } from "@apollo/client";
+import { webviewlog } from "@/commons/libraries/webview-log";
+
+const CREATE_SOLPLACE_LOG = gql`
+  mutation createSolplaceLog($createSolplaceLogInput: CreateSolplaceLogInput!) {
+    createSolplaceLog(createSolplaceLogInput: $createSolplaceLogInput) {
+      id
+      title
+      contents
+      address
+    }
+  }
+`;
+
+export const useInitialize = (methods: UseFormReturn<ISchema>) => {
+  const [createSolplaceLog] = useMutation(CREATE_SOLPLACE_LOG);
+  const router = useRouter();
+  // 쿼리스트링에서 뽑기
+  const searchParams = useSearchParams();
+  const title = searchParams.get("title");
+  const contents = searchParams.get("contents");
+  const address = searchParams.get("address");
+
+  const images = useImageStore((state) => state.images);
+  const clearImages = useImageStore((state) => state.clearImages);
+
+  const { fetchApp } = useDeviceSetting();
+
+  // 등록하기 버튼 클릭 시
+  const onSubmit = async (data: ISchema) => {
+    const { title, contents, address } = data;
+    webviewlog(data);
+    try {
+      const result = await createSolplaceLog({
+        variables: {
+          createSolplaceLogInput: {
+            title,
+            contents,
+            address,
+            images: [],
+          },
+        },
+      });
+      webviewlog(result);
+      fetchApp({ query: "requestDeviceNotificationsForPermissionSet" });
+      fetchApp({
+        query: "createDeviceNotificationsForSubmitSet",
+        variables: { page: `/solplace-logs/${result.data.id}` },
+      });
+    } catch (error) {
+      webviewlog(error);
+    }
+    clearImages();
+    router.push("/solplace-logs");
+  };
+
+  useEffect(() => {
+    if (title) methods.setValue("title", title);
+    if (images) methods.setValue("images", images);
+    if (contents) methods.setValue("contents", contents);
+    if (address) methods.setValue("address", address);
+  }, [searchParams]);
+
+  return {
+    onSubmit,
+  };
+};
