8a8ecea87c3e7b692b552268bc1dcd4b791f733f
MingseoYa <alstj3758@naver.com>
2025-02-06 23:53:41 +0900

    Feat: 솔플레이스 리스트 페이지 생성

---
+++ web/src/app/solplace-logs/page.tsx
@@ -0,0 +1,9 @@
+import SolplaceLogsList from "@/components/domains/solplace-logs-list";
+
+export default function SolplaceLogsPage() {
+  return (
+    <>
+      <SolplaceLogsList />
+    </>
+  );
+}
