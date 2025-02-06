641282705b0de529f60a7039863802d3c7ef039b
MingseoYa <alstj3758@naver.com>
2025-02-06 23:54:41 +0900

    Feat: 솔플레이스 리스트 카드 컴포넌트 생성

---
+++ web/src/components/domains/solplace-logs-list/place-card/index.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+export default function PlaceCard({ data }) {
+  return (
+    <Link className={styles.card} href={`/solplace-logs/${data.id}`}>
+      <Image
+        src={`https://storage.googleapis.com/${data.images[0]}`}
+        width={0}
+        height={0}
+        sizes="100vw"
+        className={styles.image}
+        alt="cardImage"
+      />
+      <div className={styles.contents}>
+        <div className={styles.info}>
+          <div className={styles.title}>{data.title}</div>
+          <div className={styles.description}>{data.contents}</div>
+        </div>
+        <div className={styles.address}>
+          <Image
+            className={styles.icon16}
+            src="/assets/location_icon.svg"
+            width={0}
+            height={0}
+            sizes="100vw"
+            alt="location_icon"
+          />
+          <div className={styles.addressTitle}>{data.address}</div>
+        </div>
+      </div>
+    </Link>
+  );
+}
